ec2b8534a00f60d60f4f21d3906f047c76bc614e
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-03-18 04:11:33 +0100

    chore(ci): enhance stow script to include version hashes and update workflow outputs

--- .github/scripts/stow.mjs
+++ .github/scripts/stow.mjs
@@ -5,6 +5,7 @@ const API_URL = 'https://mojistow.mojis.dev';
 export async function run() {
   console.log('running stow script');
   console.log(process.env.VERSIONS_NEEDING_UPDATE)
+  console.log(process.env.VERSIONS_WITH_HASHES)
 
   // const versions = await readdir("./data");
 
