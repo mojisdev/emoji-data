e37c14a04d281c82375120a00b92ea8fda50e003
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-03-18 04:01:52 +0100

    chore(ci): add stow script and update workflow to integrate it

---
+++ .github/scripts/stow.mjs
@@ -0,0 +1,12 @@
+// import { readdir } from "node:fs/promises"
+
+const API_URL = 'https://mojistow.mojis.dev';
+
+export async function run({ }) {
+  console.log('running stow script');
+  console.log(process.env.VERSIONS_NEEDING_UPDATE)
+
+  // const versions = await readdir("./data");
+
+
+}
