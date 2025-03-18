73af43df54579c1d0d8d3d40b954605929397dc4
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-03-18 04:04:02 +0100

    debug

--- .github/scripts/stow.mjs
+++ .github/scripts/stow.mjs
@@ -2,7 +2,7 @@
 
 const API_URL = 'https://mojistow.mojis.dev';
 
-export async function run({ }) {
+export async function run() {
   console.log('running stow script');
   console.log(process.env.VERSIONS_NEEDING_UPDATE)
 
